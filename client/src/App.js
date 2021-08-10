4cf5eac049a2d389036ceb55d866a99f10b23e4d
Borislav Borislavov <borislavkb@gmail.com>
2021-08-10 11:22:40 +0200

    Add header, main, footer to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,10 +17,13 @@ function App() {
 
   return (
     <div className="App">
+      <header className="App__header">Header</header>
+      <main>Content</main>
       <Switch>
         <Route path="/cakes">ICON 1</Route>
         <Route path="/">HOME</Route>
       </Switch>
+      <footer>Footer</footer>
     </div>
   );
 }
