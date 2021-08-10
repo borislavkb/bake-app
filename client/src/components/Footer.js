0cbe6bc2ddfd2603cf71520d816356b7f35a9a23
Borislav Borislavov <borislavkb@gmail.com>
2021-08-10 17:59:35 +0200

    Add style to Footer component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,10 @@
 import "./Footer.css";
+import { NavLink } from "react-router-dom";
 
 export default function Footer() {
-  return <div className="Footer">Home</div>;
+  return (
+    <div className="Footer">
+      <NavLink to="/">Home</NavLink>
+    </div>
+  );
 }
