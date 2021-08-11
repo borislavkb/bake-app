2709350137af117b84bc8420c235696aea1292e7
Borislav Borislavov <borislavkb@gmail.com>
2021-08-11 12:15:40 +0200

    Import icons from react-icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,12 @@
 import "./Footer.css";
 import { NavLink } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <div className="Footer">
       <NavLink className="Nav__item" to="/">
-        Home
+        <FaHome />
       </NavLink>
     </div>
   );
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,10 +1,13 @@
 import "./Navigation.css";
 import { NavLink } from "react-router-dom";
+import { CgMenuGridR } from "react-icons/cg";
 
 export default function Navigation() {
   return (
     <div className="Navigation">
-      <NavLink to="/menu">Menu</NavLink>
+      <NavLink to="/menu">
+        <CgMenuGridR />
+      </NavLink>
       <NavLink to="/intro">Intro</NavLink>
     </div>
   );
