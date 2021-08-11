929872253fce073b893c4a62031ed987b696744b
Borislav Borislavov <borislavkb@gmail.com>
2021-08-11 08:53:05 +0200

    Add style to Menu.js

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -4,12 +4,16 @@ import React from "react";
 export default function Menu() {
   return (
     <div>
-      <section className="Banner__element">
-        <h3>Get a recipe</h3>
-        <h3>Add a recipe</h3>
-        <h3>Shopping list</h3>
-        <h3>Rate a cake</h3>
-      </section>
+      <div className="Banner__element">
+        <div className="left">
+          <h3>Get a recipe</h3>
+          <h3>Add a recipe</h3>
+        </div>
+        <div className="right">
+          <h3>Shopping list</h3>
+          <h3>Rate a cake</h3>
+        </div>
+      </div>
     </div>
   );
 }
